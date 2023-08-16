89730649739350fe83acbc41904ca82291f4a24b
RaulSierradev <illosierra@outlook.com>
2023-08-16 09:23:43 -0500

    Formulario de registro de paseador + verficación de formulario

--- Client/src/Views/Registro/VerificacionPaseador.js
+++ Client/src/Views/Registro/VerificacionPaseador.js
@@ -4,13 +4,18 @@ const regexPass =
 
 export default function VerificacionPaseador(props) {
   const errores = {};
-
   if (!props.nombre.trim()) errores.name = "Este espacio está vacío";
-  if (!props.descripcion.trim()) errores.descripcion = "Este espacio está vacío";
+  if (!props.descripcion.trim())
+    errores.descripcion = "Este espacio está vacío";
   if (!props.email.trim()) errores.email = "Este espacio está vacío";
-  if (!regexEmail.test(props.email)) errores.email = 'Ingresa un email válido'
+  if (!regexEmail.test(props.email)) errores.email = "Ingresa un email válido";
   if (!props.direccion.trim()) errores.direccion = "Este espacio está vacío";
   if (!props.telefono.trim()) errores.telefono = "Este espacio está vacío";
+  if (!props.password.trim()) errores.telefono = "Este espacio está vacío";
+  if (!regexPass.test(props.password))
+    errores.password = "Contraseña incorrecta";
+  if (!props.repPassword.trim()) errores.telefono = "Este espacio está vacío";
+  if (props.password !== props.repPassword ) errores.repPassword = 'Las contraseñas no coinciden'
 
   return errores;
 }
