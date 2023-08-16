3d73710ddb03814d6665b45693482d0ba41ce076
pberus <pedroberustein03@hotmail.com>
2023-08-16 11:34:30 -0300

    .

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -4,10 +4,11 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import {Home, Nada} from "./Views"
 
 function App() {
+
   return (
     <div className='App'>
       <Routes>
-        <Route path='/' element={<Navigate to={"/inicio"} />} />
+        <Route path='/' element={<Navigate to={'/inicio'} />} />
         <Route path='/inicio' element={<Nada />} />
         <Route path='/home' element={<Home />}/>
       </Routes>
