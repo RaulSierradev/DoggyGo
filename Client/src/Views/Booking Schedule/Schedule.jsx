ae6e119470b47291d30a3fe4ff626c04d0524e67
pberus <pedroberustein03@hotmail.com>
2023-08-28 16:13:25 -0300

    .

--- Client/src/Views/Booking Schedule/Schedule.jsx
+++ Client/src/Views/Booking Schedule/Schedule.jsx
@@ -1,76 +1,66 @@
-import dayjs from "dayjs";
-import "dayjs/locale/es";
-import TextField from "@mui/material/TextField";
-import Stack from "@mui/material/Stack";
-import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
-import { DatePicker } from "@mui/x-date-pickers/DatePicker";
-import { useState } from "react";
-//import { TimePicker } from "@mui/x-date-pickers";
-import {
-  Button,
-  FormControl,
-  InputLabel,
-  MenuItem,
-  Select,
-} from "@mui/material";
+import dayjs from 'dayjs';
+import 'dayjs/locale/es';
+import TextField from '@mui/material/TextField';
+import Stack from '@mui/material/Stack';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import { useState } from 'react';
+import { TimePicker } from '@mui/x-date-pickers';
+import { Button } from '@mui/material';
+import { useDispatch, useSelector } from 'react-redux';
+import { setWalk } from '../../Redux/actions';
 
-dayjs.locale("es");
+dayjs.locale('es');
 
 const Schedule = () => {
-  const minDate = dayjs().add(1, "day")
-  const maxDate = dayjs().endOf("year");
+	const dispatch = useDispatch();
+	const walk = useSelector((state) => state.walk);
+	console.log(walk);
 
-  const [dateTimeSelected, setDateTimeSelected] = useState(minDate);
-  const [timeSelected, setTimeSelected] = useState('');
-  // const [book, setBook] = useState(false);
+	const [dateTimeSelected, setDateTimeSelected] = useState(dayjs());
+	// const [book, setBook] = useState(false);
 
-  const handleDateTimeChange = (newDate, newTime) => {
-    const combinedDateTime = newDate
-      .set("hour", newTime.hour())
-      .set("minute", newTime.minute());
-    setDateTimeSelected(combinedDateTime);
-  };
+	const handleDateTimeChange = (newDate, newTime) => {
+		const combinedDateTime = newDate
+			.set('hour', newTime.hour())
+			.set('minute', newTime.minute());
+		setDateTimeSelected(combinedDateTime);
+	};
 
-  console.log(dateTimeSelected.format("LLL"));
+	const handleChanges = () => {
+		dispatch(
+			setWalk({
+				...walk,
+				dateTimeSelected: dateTimeSelected.format('LLL'),
+			})
+		);
+		alert(`Fecha y hora seleccionada: ${dateTimeSelected.format('LLL')}`);
+	};
 
-  const handleChange = (event) => {
-    setTimeSelected(event.target.value);
-  };
+	const maxDate = dayjs().endOf('year');
+	const minTime = dayjs().isSame(dateTimeSelected, 'day') ? dayjs() : null;
 
-  return (
-    <div className='flex flex-col items-center'>
-      <label className='my-2 font-bold'>CREAR AGENDAMIENTO</label>
-      <LocalizationProvider dateAdapter={AdapterDayjs}>
-        <Stack spacing={3} direction={"row"}>
-          <DatePicker
-            label='Selecciona la fecha'
-            openTo='day'
-            views={["year", "month", "day"]}
-            minDate={minDate}
-            maxDate={maxDate}
-            value={dateTimeSelected}
-            onChange={(newDate) =>
-              handleDateTimeChange(newDate, dateTimeSelected)
-            }
-            renderInput={(params) => <TextField {...params} />}
-          />
-          <FormControl variant="outlined" sx={{width:250}}>
-            <InputLabel id='demo-simple-select-label'>Selecciona el horario</InputLabel>
-            <Select
-              labelId='demo-simple-select-label'
-              id='demo-simple-select'
-              value={timeSelected}
-              onChange={handleChange}
-              label="Selecciona el horario"
-            >
-              <MenuItem value="6 - 9 AM">6 - 9 AM</MenuItem>
-              <MenuItem value="9 - 12 AM">9 - 12 AM</MenuItem>
-              <MenuItem value="12 - 3 PM">12 - 3 PM</MenuItem>
-              <MenuItem value="3 - 6 PM">3 - 6 PM</MenuItem>
-            </Select>
-          </FormControl>
-          {/*<TimePicker
+	console.log(dateTimeSelected.format('LLL'));
+
+	return (
+		<div className="flex flex-col items-center">
+			<label className="my-2 font-bold">CREAR AGENDAMIENTO</label>
+			<LocalizationProvider dateAdapter={AdapterDayjs}>
+				<Stack spacing={3} direction={'row'}>
+					<DatePicker
+						label="Selecciona la fecha"
+						openTo="day"
+						views={['year', 'month', 'day']}
+						minDate={dayjs()}
+						maxDate={maxDate}
+						value={dateTimeSelected}
+						onChange={(newDate) =>
+							handleDateTimeChange(newDate, dateTimeSelected)
+						}
+						renderInput={(params) => <TextField {...params} />}
+					/>
+					<TimePicker
 						label="Selecciona la hora"
 						minTime={minTime}
 						minutesStep={10}
@@ -80,26 +70,19 @@ const Schedule = () => {
 							handleDateTimeChange(dateTimeSelected, newTime)
 						}
 						renderInput={(params) => <TextField {...params} />}
-					/>*/}
-        </Stack>
-      </LocalizationProvider>
-      <Stack spacing={1} direction={"row"} sx={{marginTop: 1}}>
-        {/* <Button variant="outlined">Atras</Button> */}
-          <Button
-            onClick={() =>
-              alert(
-                timeSelected === ""
-                ? "Seleccione un horario" 
-                : `Fecha y horario seleccionado: ${dateTimeSelected.format("LL")}, ${timeSelected}`
-              )
-            }
-            variant='contained'
-          >
-            Confirm
-          </Button>
-      </Stack>
-    </div>
-  );
+					/>
+				</Stack>
+			</LocalizationProvider>
+			<Stack spacing={1} direction={'row'}>
+				{/* <Button variant="outlined">Atras</Button> */}
+				<div className="mt-5">
+					<Button onClick={handleChanges} variant="contained">
+						Confirm
+					</Button>
+				</div>
+			</Stack>
+		</div>
+	);
 };
 
 export default Schedule;
