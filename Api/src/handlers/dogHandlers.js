e3e75d7469f7d18ab617b39ab7659cd880eca091
pabloferz <pablodanyfer@gmail.com>
2023-08-30 01:45:59 -0300

    put y delete Dogs

--- Api/src/handlers/dogHandlers.js
+++ Api/src/handlers/dogHandlers.js
@@ -1,4 +1,4 @@
-const { createDog, getDogs, getBreeds } = require("../controllers/dogControllers");
+const { createDog, getBreeds } = require("../controllers/dogControllers");
 const { Dog } = require("../db")
 
 const addDog = async (req, res) => {
@@ -42,10 +42,51 @@ const getAllBreeds = async (req, res) => {
     } catch (error) {
         return res.status(400).json(error.message)
     }
-
 }
+
+const updateDog = async (req, res) => {
+    try {
+        const dogID = req.params.id
+        const { name, image, size, age, recomendations, breed, sex, castrated } = req.body
+
+        await Dog.update({
+            name,
+            image,
+            size,
+            age,
+            recomendations,
+            breed,
+            sex,
+            castrated
+        }, {
+            where: {
+                id: dogID
+            }
+        })
+        return res.status(200).json("Información del perro actualizada exitosamente")
+    } catch (error) {
+        res.status(400).send("No se pudo actualizar la información del perro")
+    }
+}
+
+const deleteDog = async (req, res) => {
+    try {
+        const dogID = req.params.id
+        await Dog.destroy({
+            where: {
+                id: dogID
+            }
+        })
+        return res.status(200).send("Perro eliminado exitosamente")
+    } catch (error) {
+        return res.status(400).send("No se pudo eliminar el perro")
+    }
+}
+
 module.exports = {
     addDog,
     getAllDogs,
-    getAllBreeds
+    getAllBreeds,
+    updateDog,
+    deleteDog
 }
