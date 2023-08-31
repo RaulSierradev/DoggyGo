06bc115b814fe7db53d173578f94fa3e38e42ded
pberus <pedroberustein03@hotmail.com>
2023-08-31 17:36:02 -0300

    nuevos filtros

--- Client/src/Views/Home/Components/Filtros/Filtros.jsx
+++ Client/src/Views/Home/Components/Filtros/Filtros.jsx
@@ -1,6 +1,19 @@
 import { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { filterWalkers, restoreWalkers } from "../../../../Redux/actions";
+import {
+  Checkbox,
+  FormControl,
+  FormControlLabel,
+  IconButton,
+  InputLabel,
+  MenuItem,
+  Select,
+  Stack,
+  ToggleButton,
+  ToggleButtonGroup,
+} from "@mui/material";
+import HighlightOffIcon from "@mui/icons-material/HighlightOff";
 
 const Filtros = () => {
   const dispatch = useDispatch();
@@ -9,78 +22,93 @@ const Filtros = () => {
     dispatch(restoreWalkers());
   }, [dispatch]);
 
-  const [filter, setFilter] = useState("");
+  const [countryFilter, setCountryFilter] = useState("");
+  const [sizeFilter, setSizeFilter] = useState("");
+  const [cprFilter, setCprFilter] = useState(false);
 
-  const handleFilter = (event) => {
+  console.log(countryFilter);
+  console.log(sizeFilter);
+  console.log(cprFilter);
+
+  const handleCountryFilter = (event) => {
     event.preventDefault();
     const value = event.target.value;
 
     dispatch(filterWalkers(value));
-    setFilter(value);
+    setCountryFilter(value);
+  };
+
+  const handleCprFilter = (event) => {
+    event.preventDefault();
+    setCprFilter(event.target.checked);
+  };
+
+  const handleSizeFilter = (event, newSize) => {
+    event.preventDefault();
+    setSizeFilter(newSize);
   };
 
   const handleResetFilter = () => {
     dispatch(restoreWalkers());
-    setFilter("");
+    setCountryFilter("");
+    setSizeFilter("");
+    setCprFilter(false);
   };
 
   return (
     <div>
-      <select
-        className='bg-gray-50 border border-gray-300 text-gray-900  rounded-lg focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
-        name='locationfilter'
-        value={filter}
-        onChange={handleFilter}
-      >
-        <option value='' disabled>
-          Select Location
-        </option>
-        <optgroup label='Country'>
-          <option value='Country - Colombia'>Colombia</option>
-          <option value='Country - Argentina'>Argentina</option>
-          <option value='Country - Mexico'>Mexico</option>
-          <option value='Country - Chile'>Chile</option>
-          <option value='Country - Uruguay'>Uruguay</option>
-        </optgroup>
-        <optgroup label='State'>
-          <option value='State - Bogota D.C.'>Bogota D.C.</option>
-          <option value='State - Buenos Aires'>Buenos Aires</option>
-          <option value='State - CDMX'>CDMX</option>
-          <option value='State - Metropolitana de Santiago'>
-            Metropolitana de Santiago
-          </option>
-          <option value='State - Montevideo'>Montevideo</option>
-        </optgroup>
-        <optgroup label='City'>
-          <option value='City - Bogota'>Bogota</option>
-          <option value='City - Buenos Aires'>Buenos Aires</option>
-          <option value='City - Ciudad De Mexico'>Ciudad De Mexico</option>
-          <option value='City - Santiago'>Santiago</option>
-          <option value='City - Montevideo'>Montevideo</option>
-        </optgroup>
-      </select>
-      <button
-        type='button'
-        onClick={handleResetFilter}
-        className='bg-white border-2 rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500 mx-1'
-      >
-        <span className='sr-only'>Close menu</span>
-        <svg
-          className='h-3'
-          xmlns='http://www.w3.org/2000/svg'
-          fill='none'
-          viewBox='0 0 24 24'
-          stroke='currentColor'
-          aria-hidden='true'
-        >
-          <path
-            strokeLinecap='round'
-            strokeLinejoin='round'
-            strokeWidth='2'
-            d='M6 18L18 6M6 6l12 12'
-          />
-        </svg>
-      </button>
+      <Stack spacing={2} direction={"row"}>
+        <FormControl variant='outlined' sx={{ width: 200, top: 12 }}>
+          <InputLabel id='country-select-label'>Selecciona el pais</InputLabel>
+          <Select
+            sx={{ backgroundColor: "white" }}
+            labelId='country-select-label'
+            id='country-select'
+            value={countryFilter}
+            onChange={handleCountryFilter}
+            label='Selecciona el pais'
+          >
+            <MenuItem value='Country - Colombia'>Colombia</MenuItem>
+            <MenuItem value='Country - Argentina'>Argentina</MenuItem>
+            <MenuItem value='Country - Mexico'>Mexico</MenuItem>
+            <MenuItem value='Country - Chile'>Chile</MenuItem>
+            <MenuItem value='Country - Uruguay'>Uruguay</MenuItem>
+          </Select>
+        </FormControl>
+        <Stack spacing={1} direction={"column"}>
+          <InputLabel sx={{ fontWeight: "bold" }} id='dog-size-toggle-button-label'>
+            Tamaño del perro (kg)
+          </InputLabel>
+          <ToggleButtonGroup
+            sx={{ backgroundColor: "white" }}
+            color='primary'
+            value={sizeFilter}
+            exclusive
+            onChange={handleSizeFilter}
+          >
+            <ToggleButton value='Size - SMALL'>
+              Pequeño (3 - 10)
+            </ToggleButton>
+            <ToggleButton value='Size - MEDIUM'>Mediano (10 - 25)</ToggleButton>
+            <ToggleButton value='Size - LARGE'>Grande (25 - 45)</ToggleButton>
+            <ToggleButton value='Size - GIANT'>Gigante (45+)</ToggleButton>
+          </ToggleButtonGroup>
+        </Stack>
+        <Stack spacing={1} direction={"column"}>
+          <InputLabel sx={{ fontWeight: "bold" }} id='cpr-checkbox-label'>
+            Servicios adicionales
+          </InputLabel>
+        <FormControlLabel
+          control={<Checkbox checked={cprFilter} onChange={handleCprFilter} />}
+          label='RCP / Primeros auxilios'
+          labelPlacement='end'
+        />
+        </Stack>
+        
+        <IconButton disableRipple size='large' onClick={handleResetFilter}>
+          <HighlightOffIcon />
+        </IconButton>
+      </Stack>
     </div>
   );
 };
