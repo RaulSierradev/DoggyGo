9eefd8ce4335e3c25f8b8756bacda72dd5378f69
Mateo Meza <mezamateoj@gmail.com>
2023-09-06 13:01:51 -0300

    Add back deploy fix try number 3

--- Api/src/db.js
+++ Api/src/db.js
@@ -8,6 +8,7 @@ const sequelize = new Sequelize(DB_URL,
   {
     logging: false, // set to console.log to see the raw SQL queries
     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+    ssl: true,
   }
 );
 
