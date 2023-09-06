15894a3053345727d65f0dbf1955f095bf12beac
Mateo Meza <mezamateoj@gmail.com>
2023-09-06 13:30:22 -0300

    Add back deploy fix try number 4

--- Api/index.js
+++ Api/index.js
@@ -2,8 +2,10 @@ require("dotenv").config();
 
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
-conn.sync({ force: true }).then(() => {
-  server.listen(3001, () => {
+const PORT = process.env.PORT || 3001;
+
+conn.sync({ force: false }).then(() => {
+  server.listen(PORT, () => {
     console.log('%s listening at 3001'); // eslint-disable-line no-console
   });
 });
