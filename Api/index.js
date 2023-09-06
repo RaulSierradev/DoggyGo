f7a36b20935248987bc1ba646bae74f80b5f0f70
Mateo Meza <mezamateoj@gmail.com>
2023-09-06 12:07:17 -0300

    First try at fixing back deployment

--- Api/index.js
+++ Api/index.js
@@ -1,8 +1,10 @@
+require("dotenv").config();
+
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 conn.sync({ force: true }).then(() => {
-    server.listen(3001, () => {
-      console.log('%s listening at 3001'); // eslint-disable-line no-console
-    });
+  server.listen(3001, () => {
+    console.log('%s listening at 3001'); // eslint-disable-line no-console
   });
+});
 
