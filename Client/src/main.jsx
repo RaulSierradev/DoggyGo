479c65e0155ecde407451ec9d7dadd77113e3dcd
Mateo Meza <mezamateoj@gmail.com>
2023-09-06 17:14:17 -0300

    Add back deploy fix try number 16

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './Redux/store';
 import axios from 'axios';
@@ -11,8 +11,8 @@ axios.defaults.baseURL = 'https://doggygo-production.up.railway.app';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<Provider store={store}>
-		<BrowserRouter>
+		<HashRouter>
 			<App />
-		</BrowserRouter>
+		</HashRouter>
 	</Provider>
 );
