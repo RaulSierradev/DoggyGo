2d21198658e1728c437bb0546e9e4cda8c95ff8c
Mateo Meza <mezamateoj@gmail.com>
2023-09-06 17:07:13 -0300

    Add back deploy fix try number 15

--- Api/src/app.js
+++ Api/src/app.js
@@ -14,8 +14,6 @@ const path = require('path');
 
 
 
-
-
 require('./db.js');
 
 const server = express();
@@ -27,7 +25,7 @@ server.use(session({
   // cookie: { secure: false }
 }))
 
-// server.use(express.static(path.join(__dirname, '../../Client/dist')));
+server.use(express.static(path.join(__dirname, '../../Client/dist')));
 
 
 
@@ -85,9 +83,9 @@ server.use((req, res, next) => {
 
 server.use('/', router);
 
-// server.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, '../../Client/dist', 'index.html'));
-// });
+server.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../Client/dist', 'index.html'));
+});
 
 
 // Error catching endware.
