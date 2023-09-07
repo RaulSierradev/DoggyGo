ddac778514c95abe9d6217ce4571d9b1688e355e
Mateo Meza <mezamateoj@gmail.com>
2023-09-07 12:59:18 -0300

    add changes to landing page

--- Client/src/Components/Footer/Footer.jsx
+++ Client/src/Components/Footer/Footer.jsx
@@ -1,22 +1,33 @@
-import React from 'react'
-import style from './Footer.module.css'
+import React from 'react';
+import style from './Footer.module.css';
 
 const Footer = () => {
-  return (
-    <footer className={style.Footer} style={{marginTop:'5%'}}>
-      <div className={style.links}>
-        <h1>DoggyGo</h1>
-        <div style={{visibility:'hidden'}}>
-          <a href=''>enlace1</a>
-          <a href=''>enlace2</a>
-          <a href=''>enlace3</a>
-        </div>
-        <button style={{visibility:'hidden'}}>button</button>
-      </div>
-      <hr style={{borderColor:'rgba(51, 51, 51, 1)', width:'75%', marginLeft:'auto', marginRight:'auto', borderWidth:'1.4px', opacity:0.25}} />
-      <p className={style.Declaracion}>©2023 DoggyGo todos los derechos reservados</p>
-    </footer>
-  )
-}
+	return (
+		<footer className={style.Footer} style={{ marginTop: '0%' }}>
+			<div className={style.links}>
+				<h1>DoggyGo</h1>
+				<div style={{ visibility: 'hidden' }}>
+					<a href="">enlace1</a>
+					<a href="">enlace2</a>
+					<a href="">enlace3</a>
+				</div>
+				<button style={{ visibility: 'hidden' }}>button</button>
+			</div>
+			<hr
+				style={{
+					borderColor: 'rgba(51, 51, 51, 1)',
+					width: '75%',
+					marginLeft: 'auto',
+					marginRight: 'auto',
+					borderWidth: '1.4px',
+					opacity: 0.25,
+				}}
+			/>
+			<p className={style.Declaracion}>
+				©2023 DoggyGo todos los derechos reservados
+			</p>
+		</footer>
+	);
+};
 
-export default Footer
+export default Footer;
--- Client/src/Views/Landing/Landing.jsx
+++ Client/src/Views/Landing/Landing.jsx
@@ -1,6 +1,6 @@
 import style from './Landing.module.css';
 import Paseadores from '../../Components/Paseadores/Paseadores';
-import { useNavigate } from 'react-router-dom';
+import { NavLink, useNavigate } from 'react-router-dom';
 import Nav from '../Nav';
 import Footer from '../../Components/Footer/Footer';
 //import Footer from '../Dashboard/components/Footer';
@@ -19,25 +19,25 @@ const Landing = () => {
 	//requerido de redux
 	const PaseadoresEstrella = [
 		{
-			img: 'https://picsum.photos/203',
-			price: 'paseador1',
-			addres: 'paseador1',
-			dogs: 'paseador1',
-			distance: 'paseador1',
+			img: 'https://images.unsplash.com/photo-1612981811649-f304acad6425?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8ZG9nJTIwd2Fsa2VyfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60',
+			price: 'Diego',
+			addres: 'Santiago de Chile',
+			dogs: 'Paseos completados',
+			distance: '200',
 		},
 		{
-			img: 'https://picsum.photos/210',
-			price: 'paseador2',
-			addres: 'paseador2',
-			dogs: 'paseador2',
-			distance: 'paseador2',
+			img: 'https://images.unsplash.com/photo-1484863137850-59afcfe05386?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzJ8fHBlb3BsZXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60',
+			price: 'Martina',
+			addres: 'Buenos Aires',
+			dogs: 'Paseos completados',
+			distance: '100',
 		},
 		{
-			img: 'https://picsum.photos/200',
-			price: 'paseador3',
-			addres: 'paseador3',
-			dogs: 'paseador3',
-			distance: 'paseador3',
+			img: 'https://images.unsplash.com/photo-1522529599102-193c0d76b5b6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDl8fHBlb3BsZXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60',
+			price: 'Bruno',
+			addres: 'California',
+			dogs: 'Pasos completados',
+			distance: '50',
 		},
 	];
 
@@ -53,11 +53,18 @@ const Landing = () => {
 				<span>Bienvenido a Doggy</span>
 				<h2>Pasea a tu perro</h2>
 				<p>
-					Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-					Animi ex, omnis ea blanditiis odit incidunt architecto
-					numquam magni veritatis aliquam, sed ut accusantium illo
-					unde cupiditate itaque harum adipisci quidem?
+					Sabemos lo ocupada que puede ser la vida moderna y cuán
+					importante es el bienestar de tu peludo amigo. DoggyGo llega
+					para solucionar esos problemas, ofreciéndote una forma
+					fácil, rápida y segura de encontrar paseadores de perros
+					confiables cerca de ti.
 				</p>
+				<NavLink
+					to={'/registro'}
+					className="bg-sky-900 rounded-md p-2 text-white font-bold"
+				>
+					Unete Hoy
+				</NavLink>
 			</section>
 			<section className={style.sec2}>
 				<p className={style.sec2txP}>¿como funciona?</p>
@@ -73,9 +80,9 @@ const Landing = () => {
 					<h2 className={style.sec3tll}>paseadores estrella</h2>
 					<div>
 						<p className={style.sec3stx}>
-							de entre nuestros mejores paseadores...
+							Algunos de nuestros mejores paseadores...
 						</p>
-						<button className={style.sec3vwall}>
+						<NavLink to={'/home'} className={style.sec3vwall}>
 							ver todos los paseadores{' '}
 							<img
 								style={{
@@ -85,7 +92,7 @@ const Landing = () => {
 								src={flecha}
 								alt=""
 							/>
-						</button>
+						</NavLink>
 					</div>
 				</div>
 				<div className={style.sec3PSContainer}>
