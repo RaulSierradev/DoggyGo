8e5ce431c4d62ba9891210a97d33a14877f45214
Mateo Meza <mezamateoj@gmail.com>
2023-09-07 12:09:53 -0300

    add test changes 4

--- Client/src/Views/Admin/Clients/CardsClients.jsx
+++ Client/src/Views/Admin/Clients/CardsClients.jsx
@@ -1,55 +1,60 @@
-import { Link } from "react-router-dom";
-import { useState } from "react";
-import PaginadoAdmin from "../PaginadoAdmin";
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import PaginadoAdmin from '../PaginadoAdmin';
 
 const CardsClients = (props) => {
-  const { clients } = props;
-  const [currentPage, setCurrentPage] = useState(1);
-  const cardsPerPage = 6;
+	const { clients } = props;
+	const [currentPage, setCurrentPage] = useState(1);
+	const cardsPerPage = 6;
 
-  const totalCards = clients.length;
+	const totalCards = clients.length;
 
-  const lastIndex = currentPage * cardsPerPage;
-  const firstIndex = lastIndex - cardsPerPage;
-  return (
-    <div className="mt-16">
-      <table className="m-10 text-center">
-        <tr className="p-10">
-          <td className="font-bold text-xl text-indigo-800">Nombre</td>
-          <td className="font-bold text-xl text-indigo-800">Correo</td>
-          <td className="font-bold text-xl text-indigo-800">Telefono</td>
-          <td className="font-bold text-xl text-indigo-800">Pais</td>
-        </tr>
-        {clients
-          ?.map((e, index) => {
-            return (
-              <tr key={index}>
-                <td className="px-5 py-3">{e.name}</td>
-                <td className="px-5">{e.email}</td>
-                <td className="px-5">{e.phone}</td>
-                <td className="px-5">{e.country}</td>
-                <td className="px-10">
-                  <Link
-                    className="bg-green-600 p-2 text-sm text-white font-semibold rounded-lg hover:bg-green-500"
-                    to={`http://localhost:5173/admin/detail/${e.id}`}
-                  >
-                    Ver perfil
-                  </Link>
-                </td>
-                
-              </tr>
-            );
-          })
-          .slice(firstIndex, lastIndex)}
-      </table>
-      <PaginadoAdmin
-        cardsPerPage={cardsPerPage}
-        currentPage={currentPage}
-        setCurrentPage={setCurrentPage}
-        totalCards={totalCards}
-      />
-    </div>
-  );
+	const lastIndex = currentPage * cardsPerPage;
+	const firstIndex = lastIndex - cardsPerPage;
+	return (
+		<div className="mt-16">
+			<table className="m-10 text-center">
+				<tr className="p-10">
+					<td className="font-bold text-xl text-indigo-800">
+						Nombre
+					</td>
+					<td className="font-bold text-xl text-indigo-800">
+						Correo
+					</td>
+					<td className="font-bold text-xl text-indigo-800">
+						Telefono
+					</td>
+					<td className="font-bold text-xl text-indigo-800">Pais</td>
+				</tr>
+				{clients
+					?.map((e, index) => {
+						return (
+							<tr key={index}>
+								<td className="px-5 py-3">{e.name}</td>
+								<td className="px-5">{e.email}</td>
+								<td className="px-5">{e.phone}</td>
+								<td className="px-5">{e.country}</td>
+								<td className="px-10">
+									<Link
+										className="bg-green-600 p-2 text-sm text-white font-semibold rounded-lg hover:bg-green-500"
+										to={`https://doggy-go.onrender.com/admin/detail/${e.id}`}
+									>
+										Ver perfil
+									</Link>
+								</td>
+							</tr>
+						);
+					})
+					.slice(firstIndex, lastIndex)}
+			</table>
+			<PaginadoAdmin
+				cardsPerPage={cardsPerPage}
+				currentPage={currentPage}
+				setCurrentPage={setCurrentPage}
+				totalCards={totalCards}
+			/>
+		</div>
+	);
 };
 
 export default CardsClients;
